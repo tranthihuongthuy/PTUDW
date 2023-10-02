9e664b093f34e3fe5e7141da245444729b2de7f4
thirr <tranthihuongthuy27@gmail.com>
2023-10-02 16:18:59 +0700

    sql

--- 63CNTT5_N1/Controllers/SiteController.cs
+++ 63CNTT5_N1/Controllers/SiteController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MyClass.Model;
 namespace _63CNTT5_N1.Controllers
 {
     public class SiteController : Controller
@@ -11,6 +11,9 @@ namespace _63CNTT5_N1.Controllers
         // GET: Site
         public ActionResult Index()
         {
+            MyDBContext db = new MyDBContext();
+            int Count = db.Products.Count();
+            ViewBag.choi = Count;
             return View();
         }
     }
